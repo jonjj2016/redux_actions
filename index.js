713de3587b92ada26cb2e487d6fc54861a837d26
jonJJ2016 <jonmartirosyan1988@gmail.com>
2022-10-03 03:45:58 +0800

    added some changes

--- index.js
+++ index.js
@@ -22,20 +22,13 @@ const actionGen = (arr) => {
   return actionObj
 }
 
-const typeGen = (constant, optional, bool, vars) => {
+const newTypeGen = ({ constant, crud, loading, action }) => {
   let Types = ['CREATE', 'PATCH', 'REMOVE', 'GET', 'FIND']
-  let TypeArr = []
-  if (optional) {
-    //if bool is true means we don't need default crud types
-    optional = optional.map((option) => {
-      return option.toUpperCase()
-    })
-    if (!bool) {
-      Types = [...optional]
-    } else {
-      Types = [...Types, ...optional]
-    }
+  if (!crud && action) {
+    console.log(crud)
+    Types = [action]
   }
+  let TypeArr = []
 
   let obj = {}
   Types.forEach((item) => {
@@ -43,24 +36,14 @@ const typeGen = (constant, optional, bool, vars) => {
       [`${item}_START`]: `${item}_${constant}_START`,
       [`${item}_SUCCESS`]: `${item}_${constant}_SUCCESS`,
       [`${item}_FAILED`]: `${item}_${constant}_FAILED`,
-      [`LOADING`]: `${constant}_LOADING`,
-      [`SET_CURRENT`]: `${constant}_SET_CURRENT`,
     }
-    if (!bool && vars) {
-      block = {}
-      vars.forEach((variable) => {
-        block = {
-          ...block,
-          // [`${variable.toUpperCase()}`]: `${item}_${variable.toUpperCase()}`
-          [`${item}_${variable.toUpperCase()}`]: `${item}_${variable.toUpperCase()}`,
-        }
-      })
+    if (loading) {
+      block[`${item}_LOADING`] = `${item}_${constant}_LOADING`
     }
+
     obj = {
       ...obj,
       ...block,
-      // [`LOADING`]: `${item}_LOADING`,
-      // [`SET_CURRENT`]: `${item}_SET_CURRENT`,
     }
   })
   TypeArr = {
@@ -69,79 +52,30 @@ const typeGen = (constant, optional, bool, vars) => {
 
   return TypeArr
 }
-//   const autoGen = (arr, optional, bool) => {
-//     // return actionGen([typeGen(arr, optional, bool)])
-//     const arg = arr.toUpperCase();
-//     const types = typeGen(arg, optional, bool);
-//     const actions = actionGen(types);
 
-//     return [actions, types];
-//   };
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-const autoTypeGen = (con, ...myArr) => {
-  let newConstant = con.toUpperCase()
-  if (myArr.length === 0) {
-    const types = typeGen(newConstant)
-    const actions = actionGen(types)
-    return [types, actions]
-  }
-  // if(!myArr)
-  if (typeof myArr[myArr.length - 1] === 'boolean') {
-    myArr.pop()
-    const types = typeGen(newConstant, myArr, true)
-    const actions = actionGen(types)
-    return [types, actions]
-  }
-
-  const types = typeGen(newConstant, myArr, false)
-  const actions = actionGen(types)
-  return [types, actions]
-}
-const customTypeGen = (option, ...options) => {
-  const types = typeGen(null, [option], null, [...options])
-  const actions = actionGen(types)
-  return [types, actions]
-}
-
-const _sympletypeGen = (constant, optional, bool, vars) => {
-  let Types = ['CREATE', 'PATCH', 'REMOVE', 'GET', 'FIND']
+const custypeGen = (optional, vars) => {
+  let Types = []
   let TypeArr = []
   if (optional) {
-    //if bool is true means we don't need default crud types
     optional = optional.map((option) => {
       return option.toUpperCase()
     })
-    if (!bool) {
-      Types = [...optional]
-    } else {
-      Types = [...Types, ...optional]
-    }
+
+    Types = [...Types, ...optional]
   }
 
   let obj = {}
   Types.forEach((item) => {
-    let block = {
-      [`${item}`]: `${item}_${constant}`,
+    let block = {}
+    vars.forEach((variable) => {
+      block = {
+        ...block,
+        [`${item}_${variable.toUpperCase()}`]: `${item}_${variable.toUpperCase()}`,
+      }
+    })
 
-      [`LOADING`]: `${constant}_LOADING`,
-      [`SET_CURRENT`]: `${constant}_SET_CURRENT`,
-    }
-    if (!bool && vars) {
-      block = {}
-      vars.forEach((variable) => {
-        block = {
-          ...block,
-          // [`${variable.toUpperCase()}`]: `${item}_${variable.toUpperCase()}`
-          [`${item}_${variable.toUpperCase()}`]: `${item}_${variable.toUpperCase()}`,
-        }
-      })
-    }
     obj = {
-      ...obj,
       ...block,
-      // [`LOADING`]: `${item}_LOADING`,
-      // [`SET_CURRENT`]: `${item}_SET_CURRENT`,
     }
   })
   TypeArr = {
@@ -150,29 +84,26 @@ const _sympletypeGen = (constant, optional, bool, vars) => {
 
   return TypeArr
 }
-
-const simpleTypeGen = (con, ...myArr) => {
-  let newConstant = con.toUpperCase()
-  if (myArr.length === 0) {
-    const types = _sympletypeGen(newConstant)
-    const actions = actionGen(types)
-    return [types, actions]
+const initActions = ({ type, action = '', crud = false, loading = false }) => {
+  if (!type) {
+    throw new Error('Type is required')
   }
-  // if(!myArr)
-  if (typeof myArr[myArr.length - 1] === 'boolean') {
-    myArr.pop()
-    const types = _sympletypeGen(newConstant, myArr, true)
-    const actions = actionGen(types)
-    return [types, actions]
-  }
-
-  const types = _sympletypeGen(newConstant, myArr, false)
+  const types = newTypeGen({
+    constant: type.toUpperCase(),
+    type,
+    crud,
+    loading,
+    action: action.toUpperCase(),
+  })
   const actions = actionGen(types)
   return [types, actions]
 }
-
+const customTypeGen = ({ type, actions }) => {
+  const types = custypeGen([type], actions)
+  const customActions = actionGen(types)
+  return [types, customActions]
+}
 module.exports = {
   customTypeGen,
-  autoTypeGen,
-  simpleTypeGen,
+  initActions,
 }
